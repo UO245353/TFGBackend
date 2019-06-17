3c6f70aa20e5179b9e6bf487eb4dd012168b9bbd
Daniel Alvarez Vega <wave10496@gmail.com>
2019-06-17 09:44:59 +0200

    alexa init

--- endpoint/api/alexa/alexaEndpoint.js
+++ endpoint/api/alexa/alexaEndpoint.js
@@ -1,1 +1,44 @@
 'use strict';
+
+module.exports = app => {
+
+  const Alexa = require('ask-sdk-v1adapter');
+  const bodyParser = require('body-parser');
+  const alexaVerifier = require('alexa-verifier-middleware');
+  const alexaContext = require('aws-lambda-mock-context');
+
+  const _ = app._;
+  const Promise = app.promise;
+
+  const debugActive = true;
+
+  function debug (tag, message) {
+    if (!debugActive) {
+      return;
+    }
+    if (!message) {
+      return;
+    }
+    if (tag) {
+      console.log(tag, message);
+    }
+    else {
+      console.log(message);
+    }
+  }
+
+  app.post('/alexa',
+  (req, res, next) => {
+
+    debug('REQUEST', req);
+
+    next();
+  },
+  alexaVerifier,
+  bodyParser.json(),
+  (req, res) => {
+
+    return res.status(500).json({});
+  });
+
+};
