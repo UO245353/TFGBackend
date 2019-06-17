38322bac28190f3bc14d560640422d8fc011ba61
Daniel Alvarez Vega <wave10496@gmail.com>
2019-06-17 15:04:29 +0200

    alexa things

--- endpoint/api/alexa/alexaEndpoint.js
+++ endpoint/api/alexa/alexaEndpoint.js
@@ -7,6 +7,8 @@ module.exports = app => {
   const alexaVerifier = require('alexa-verifier-middleware');
   const alexaContext = require('aws-lambda-mock-context');
 
+  const alexaLib = require('../../../lib/alexaLib')(app);
+
   const _ = app._;
   const Promise = app.promise;
 
@@ -27,16 +29,60 @@ module.exports = app => {
     }
   }
 
+  const handlerDriver = {
+    //Generic handlers
+    'LaunchRequest': function () {
+
+      return alexaLib.launch(this);
+    },
+    'AMAZON.HelpIntent': function () {
+
+      return alexaLib.help(this);
+    },
+    'AMAZON.CancelIntent': function () {
+
+      return alexaLib.cancel(this);
+    },
+    'AMAZON.StopIntent': function () {
+
+      return alexaLib.cancel(this);
+    },
+    'Unhandled': function () {
+
+      return this.emit(
+        ':ask',
+        alexaLib.alexaResponses.NOT_SUPPORTED
+      );
+    },
+    'Speak': function (text) {
+
+      return this.emit(':tell', text);
+    }
+  };
+
   app.post('/alexa',
   alexaVerifier,
   bodyParser.json(),
   (req, res) => {
 
-    debug('REQUEST BODY KEYS', Object.keys(req.body));
-
     debug('REQUEST BODY', req.body);
 
-    return res.status(500).json({});
+    let context = alexaContext();
+
+    context.Promise
+    .then(resp => res.status(200).json(resp) )
+    .catch(err => {
+      console.log(req.body.request);
+      console.log(err);
+      return res.status(400).json({error: err});
+    });
+
+    let event = req.body;
+    let alexaHandler = Alexa.handler(event, context);
+
+    alexaHandler.registerHandlers(handlerDriver);
+
+    return alexaHandler.execute();
   });
 
 };
